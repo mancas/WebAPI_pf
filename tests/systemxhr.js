7a97ff67afa70f407d155ae74fc8a7dfc71bfbb5
Manuel Casas <manuel.casasbarrado@gmail.com>
2015-06-03 15:02:18 +0200

    tests

--- tests/systemxhr.js
+++ tests/systemxhr.js
@@ -12,13 +12,18 @@
       var log = window.Tests.log.bind(undefined, 'systemxhr');
       var abort = window.Tests.abort;
 
-      try {
-        log('Starting systemXHR polyfill tests');
+      function createXMLHttpRequest() {
         var newXHR = new XMLHttpRequest({ mozSystem: true });
         newXHR._updateXMLHttpRequestObject ||
           abort('polyfill window.XMLHttpRequest not defined.');
 
         log('polyfill window.XMLHttpRequest defined!');
+
+        return newXHR;
+      }
+
+      function testXMLHttpRequest() {
+        var newXHR = createXMLHttpRequest();
 
         newXHR.onreadystatechange = evt => console.info(evt);
         newXHR.open('get',
@@ -40,7 +45,38 @@
             log('XHR error. ' + newXHR.statusText);
           }
         };
+      }
 
+      function testXMLHttpRequestJSON() {
+        var newXHR = createXMLHttpRequest();
+
+        newXHR.onreadystatechange = evt => console.info(evt);
+        newXHR.open('get',
+                    'http://antonioma.github.io/WebAPI_pf/tests/test_list.json',
+                    true);
+        newXHR.responseType = 'json';
+        newXHR.send();
+
+        newXHR.onload = function() {
+          log('Throw onload');
+          if (newXHR.status === 200) {
+            if (newXHR.response)
+              log ('We got a response: ' + newXHR.response);
+              if (Object.keys(newXHR.response).length > 0) {
+                log('And it has data');
+              } else {
+                log('but it has not data');
+              }
+          } else {
+            log('XHR error. ' + newXHR.statusText);
+          }
+        };
+      }
+
+      try {
+        log('Starting systemXHR polyfill tests');
+        testXMLHttpRequest();
+        testXMLHttpRequestJSON();
       } catch (e) {
         log("Finished early with " + e);
       }
